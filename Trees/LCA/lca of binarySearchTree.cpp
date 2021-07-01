9a77a54fbfd5716f4557fec1b0951569fd7b86c0
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2021-07-01 20:56:40 +0000

    lca in bst

--- Trees/LCA/lca of binarySearchTree.cpp
+++ Trees/LCA/lca of binarySearchTree.cpp
@@ -1,82 +1,34 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
+ * struct TreeNode {
  *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
-
-/*
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // recursive sol by using property of bst
-        
-           // Value of current node or parent node.
-        int parentVal = root.val;
-
-        // Value of p
-        int pVal = p.val;
-
-        // Value of q;
-        int qVal = q.val;
-
-        if (pVal > parentVal && qVal > parentVal) {
-            // If both p and q are greater than parent
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (pVal < parentVal && qVal < parentVal) {
-            // If both p and q are lesser than parent
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
-            // We have found the split point, i.e. the LCA node.
-            return root;
-        }
-    }
-}
-
-*/
 
 class Solution
 {
-public
-    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
 
-        // iterative sol by using property of bst
+        if (!root)
+            return NULL; // base condition
 
-        // Value of p
-        int pVal = p.val;
+        if ((root == p) || (root == q)) // recursive check condition when we find either of them
+            return root;
 
-        // Value of q;
-        int qVal = q.val;
+        TreeNode *lca1 = (lowestCommonAncestor(root->left, p, q)); // when in either or same for checking that
+        TreeNode *lca2 = (lowestCommonAncestor(root->right, p, q));
 
-        // Start from the root node of the tree
-        TreeNode node = root;
+        if ((lca1) && (lca2))
+            return root; // when present in either of subtree
 
-        // Traverse the tree
-        while (node != null)
-        {
-
-            // Value of ancestor/parent node.
-            int parentVal = node.val;
-
-            if (pVal > parentVal && qVal > parentVal)
-            {
-                // If both p and q are greater than parent
-                node = node.right;
-            }
-            else if (pVal < parentVal && qVal < parentVal)
-            {
-                // If both p and q are lesser than parent
-                node = node.left;
-            }
-            else
-            {
-                // We have found the split point, i.e. the LCA node.
-                return node;
-            }
-        }
-        return null;
+        if (lca1)
+            return lca1; // when prsent in both
+        else
+            return lca2;
     }
-}
+};
