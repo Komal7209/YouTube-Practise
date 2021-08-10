4fcf1a425daa1b3c83166ccda5df9358e6410e9c
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2021-08-10 03:08:29 +0000

    strings

--- leetcode aug challenge 2021/Add Two Strings/javaSol.java
+++ leetcode aug challenge 2021/Add Two Strings/javaSol.java
@@ -3,10 +3,10 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int carry = 0;
         for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
-            int x = i < 0 ? 0 : num1.charAt(i) - '0';
-            int y = j < 0 ? 0 : num2.charAt(j) - '0';
-            sb.append((x + y + carry) % 10);
-            carry = (x + y + carry) / 10;
+            int digit1 = i < 0 ? 0 : num1.charAt(i) - '0';
+            int digit2 = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((digit1 + digit2 + carry) % 10);
+            carry = (digit1 + digit2 + carry) / 10;
         }
         return sb.reverse().toString();
     }
--- leetcode aug challenge 2021/Add Two Strings/pythonSol.py
+++ leetcode aug challenge 2021/Add Two Strings/pythonSol.py
@@ -8,10 +8,10 @@ class Solution(object):
         num1, num2 = list(num1), list(num2)
         carry, res = 0, []
         while len(num2) > 0 or len(num1) > 0:
-            n1 = ord(num1.pop())-ord('0') if len(num1) > 0 else 0
-            n2 = ord(num2.pop())-ord('0') if len(num2) > 0 else 0
+            digit1 = ord(num1.pop())-ord('0') if len(num1) > 0 else 0
+            digit2 = ord(num2.pop())-ord('0') if len(num2) > 0 else 0
             
-            temp = n1 + n2 + carry 
+            temp = digit1 + digit2 + carry 
             res.append(temp % 10)
             carry = temp // 10
         if carry: res.append(carry)
