a83061988f8f791e565d6adf172dc74f09da8a85
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2021-12-05 12:55:44 +0000

    HouseRobber3

---
+++ Leetcode_December_Daily_Challenge_2021/HouseRobbers3/Sol.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  int rob(TreeNode* root) {
+    int robNotMax = 0;
+    return helper(root, robNotMax);
+}
+
+int helper(TreeNode* root, int& robNotMax)
+{
+    
+    if (!root) return 0;
+    
+    int leftNotRobMax = 0, rightNotRobMax = 0;
+    
+    int leftMax     = helper(root->left,  leftNotRobMax);
+    int rightMax    = helper(root->right, rightNotRobMax);
+    
+    int robMax = root->val + leftNotRobMax + rightNotRobMax;
+    robNotMax = leftMax + rightMax;
+    
+    return max (robMax, robNotMax);
+}
+};
