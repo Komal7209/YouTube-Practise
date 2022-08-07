e83e0f7cd26cbadce6b7a7fcf28e66b52f6d3807
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2022-08-07 06:13:23 +0000

    sol1

---
+++ LeetcodeContests/LeetcodeWeeklyContests/Contest-305/Sol1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        
+        int res=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[j]-nums[i]==diff)
+                for(int k=j+1;k<nums.size();k++)
+                {
+                    if(nums[k]-nums[j]==diff)
+                        res++;
+                }
+            }
+        }
+        return res;
+    }
+};
