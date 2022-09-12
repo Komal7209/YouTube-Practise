d1a3f1c69e519f201a70d2609a41a7354fdc162b
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2022-09-12 03:29:57 +0000

    Day_12

---
+++ Leetcode_September_2022_Daily_Challenges/Day_12/Sol.cpp
@@ -0,0 +1,34 @@
+// As we can choose any token thus we can sort
+// sorting will make easy to choose greater token so that we can use that power at later point of time to increase score
+// if we have sufficient power to increase score then we can use our power
+
+// TC: O(nlogn)
+// SC: O(1)
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int left =0, right = tokens.size()-1, score =0, ans =0;
+        sort(tokens.begin(), tokens.end());
+        
+        while(left <= right)
+        {
+            if(tokens[left] <= power)
+            {
+                score++;
+                power -= tokens[left];
+                left++;
+                ans = max(ans, score);
+            }
+          else if(tokens[left] > power and score >0)
+            {
+                power += tokens[right];
+                right--;
+                score--;
+            }
+            else 
+                return score;
+        }
+        return ans;
+    }
+};
