d125b80015353410a54d060699520ab19434e228
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2022-09-17 17:25:28 +0000

    biweekly contest

---
+++ LeetcodeContests/LeetcodeBiweeklyContests/Contest-87/Sol2.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    public:
+int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+
+        int j = 0, ans = 0, maxIndex = 0;
+
+        for(int i = 0; i < players.size() && j <trainers.size(); i++) {
+            int need = players[i];
+
+            while(trainers[j] < need) {
+                j++;
+                if(j == trainers.size()) return ans;
+            }
+
+            if(trainers[j] >= need) {
+                ans++;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
--- Leetcode_September_2022_Daily_Challenges/Day_14/Sol.cpp
+++ Leetcode_September_2022_Daily_Challenges/Day_14/Sol.cpp
@@ -1,16 +1,21 @@
-class Solution {
-    private: 
-        int count =0;
-    
-     void preorder(TreeNode* node, int path) {
-        if (node) {
-            // compute occurences of each digit 
+class Solution
+{
+private:
+    int count = 0;
+
+    void preorder(TreeNode *node, int path)
+    {
+        if (node)
+        {
+            // compute occurences of each digit
             // in the corresponding register
             path = path ^ (1 << node->val);
             // if it's a leaf check if the path is pseudo-palindromic
-            if (!node->left && !node->right) {
+            if (!node->left && !node->right)
+            {
                 // check if at most one digit has an odd frequency
-                if ((path & (path - 1)) == 0) {
+                if ((path & (path - 1)) == 0)
+                {
                     ++count;
                 }
             }
@@ -18,10 +23,11 @@ class Solution {
             preorder(node->right, path);
         }
     }
+
 public:
-    int pseudoPalindromicPaths (TreeNode* root) {
+    int pseudoPalindromicPaths(TreeNode *root)
+    {
         preorder(root, 0);
-        return count; 
+        return count;
     }
 };
-
