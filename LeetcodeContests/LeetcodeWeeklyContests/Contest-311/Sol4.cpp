3aa854a710d6e65e2b529497a1e51370875d88f1
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2022-09-18 16:33:10 +0000

    formatted

--- LeetcodeContests/LeetcodeWeeklyContests/Contest-311/Sol4.cpp
+++ LeetcodeContests/LeetcodeWeeklyContests/Contest-311/Sol4.cpp
@@ -1,48 +1,57 @@
-struct TrieNode {
+struct TrieNode
+{
     TrieNode *next[26] = {};
     int cnt = 0;
 };
 
-class Solution {
-    private:
-    
+class Solution
+{
+private:
     TrieNode root;
-    
-    void insert(string word) {
+
+    void insert(string word)
+    {
         auto node = &root;
-        for(char c : word) {
-            if(!node->next[c - 'a']) {
+        for (char c : word)
+        {
+            if (!node->next[c - 'a'])
+            {
                 node->next[c - 'a'] = new TrieNode();
-            } 
+            }
             node->next[c - 'a']->cnt++;
-            node = node->next[c - 'a'];   
+            node = node->next[c - 'a'];
         }
     }
 
-    int prefixCnt(string s) {
+    int prefixCnt(string s)
+    {
         auto node = &root;
         int ans = 0;
-        for(char c : s) {
+        for (char c : s)
+        {
             ans += node->next[c - 'a']->cnt;
             node = node->next[c - 'a'];
         }
         return ans;
     }
-    
+
 public:
-  
-    vector<int> sumPrefixScores(vector<string>& words) {
-        
-		// Insert words in trie.
-        for (int i = 0; i < words.size(); i++) {
-                insert(words[i]);
+    vector<int> sumPrefixScores(vector<string> &words)
+    {
+
+        // Insert words in trie.
+        for (int i = 0; i < words.size(); i++)
+        {
+            insert(words[i]);
         }
         vector<int> ans(words.size(), 0);
-        
-        for (int i = 0; i < words.size(); i++) {
-			// Get the count of all prefixes of given string.
-            ans[i] = prefixCnt(words[i]); 
+
+        for (int i = 0; i < words.size(); i++)
+        {
+            // Get the count of all prefixes of given string.
+            ans[i] = prefixCnt(words[i]);
         }
         return ans;
     }
 };
+
