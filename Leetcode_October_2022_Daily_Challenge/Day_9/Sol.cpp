bb46e7d11b8be5fd0ac12c2c8acaea4966558fde
Komal Pal <33154959+Komal7209@users.noreply.github.com>
2022-10-09 07:01:14 +0000

    DAy_9

---
+++ Leetcode_October_2022_Daily_Challenge/Day_9/Sol.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    private: unordered_set<int> s;
+public:     
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        if (s.count(k - root->val)) return true;
+        s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
+
