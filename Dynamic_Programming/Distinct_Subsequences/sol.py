2181b1bd283180b3ea43e2c68af150f8a5af88fd
Komal Pal <komal.pal_7@yahoo.com>
2023-10-07 13:21:13 +0530

    Create sol.py

---
+++ Dynamic_Programming/Distinct_Subsequences/sol.py
@@ -0,0 +1,12 @@
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = 1
+        for j in range(n+1):
+            dp[0][j] = 0
+        dp[0][0] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1] * (s[i-1]==t[j-1])
+        return dp[m][n]
